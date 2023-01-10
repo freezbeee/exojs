703beb312c84e11b75f6e0e98bdc7b8046b7c1f2
theo courbot <michelle.marco@gmail.com>
2023-01-10 12:02:16 +0100

    1

--- exo 2/assets/main.js
+++
@@ -1,7 +0,0 @@
-let recup = prompt("votre 1er nombre : "); // let recup = NUMBER prompt("votre 1er nombre : ");
-let recup2 = prompt("votre 2eme nombre : "); //let recup2 = NUMBER prompt("votre 2eme nombre : ");
-let result = parseInt(recup) + parseInt(recup2); //let result = recup + recup2;
-
-let phrase = `  ${recup} + ${recup2} = ${result}`
-
-console.log(phrase)

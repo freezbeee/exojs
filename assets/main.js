483c6caaab2c9c6c1f6de69ce6d20e0f20d109b7
mich et marc <michelle.marco@gmail.com>
2023-01-10 11:07:19 +0100

    corection

--- assets/main.js
+++ assets/main.js
@@ -4,6 +4,6 @@ let recup2 = prompt("votre prenom utilisateur 2 : ");
 let recupLastName2 = prompt("votre nom utilisateur 2 : ");
 
 let phrase = ` bonjour ${recup} ${recupLastName} et bojour a toi aussi 
-${recup2} ${recupLastName2} bivenue`
+${recup2} ${recupLastName2} bienvenue`
 
 console.log(phrase)

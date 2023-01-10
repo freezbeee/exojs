10443faccec0e82dcd08e8d53c4ae037ef13378e
theo courbot <michelle.marco@gmail.com>
2023-01-10 12:13:53 +0100

    1

--- exo3/assets/main.js
+++ exo3/assets/main.js
@@ -3,6 +3,6 @@ let rayon = parseInt(diametre) / 2;
 let result =2*Math.PI*rayon
 let resultaire = Math.PI*Math.pow(rayon,2)
 
-let phrase = `  le perimetre d'un cercle de ${diametre} cm est ${result} cm et l'aire est ${resultair} cm25`
+let phrase = `  le perimetre d'un cercle de ${diametre} cm est ${result} cm et l'aire est ${resultaire} cm2`
 
 console.log(phrase)

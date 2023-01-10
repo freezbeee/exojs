3594e464c895fcb89c6a4800faacbbe441fd04eb
theo courbot <michelle.marco@gmail.com>
2023-01-10 17:10:14 +0100

    1

---
+++ exo7 condition/assets/main.js
@@ -0,0 +1,15 @@
+let mail =  (prompt("donner votre mail ")); 
+let mdp =  (prompt("donner lvotre mdp ")); 
+
+
+let mailbdd = "coucou@moi.fr"
+let mdpbdd = "coucou"
+
+
+if((mail === mailbdd) && (mdp === mdpbdd)){
+    console.log("bienvenue sur votre compte")
+
+} else {
+    console.log("information incorect")
+}
+

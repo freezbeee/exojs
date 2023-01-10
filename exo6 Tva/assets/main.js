9a2fadafbea9647f3a0f04c6f89b96ddbdc0db83
theo courbot <michelle.marco@gmail.com>
2023-01-10 14:20:26 +0100

    1

---
+++ exo6 Tva/assets/main.js
@@ -0,0 +1,11 @@
+let priceht = Number (prompt("donner le prix hors taxe en euros: ")); 
+let tvapourcent = Number (prompt("donner la T.V.A en %: ")); 
+let tva =  (priceht * tvapourcent) / 100 ;
+let pricettc = priceht + tva
+
+
+let phrase1 = ` vous allez payer ${pricettc} euros `
+
+console.log(phrase1)
+
+

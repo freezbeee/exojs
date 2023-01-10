7b970fcd7bf942792414e28c0b7a3a1e38c6c7ac
theo courbot <michelle.marco@gmail.com>
2023-01-10 17:17:10 +0100

    1

--- exo7 condition/assets/main.js
+++ exo7 condition/assets/main.js
@@ -5,11 +5,20 @@ let mdp =  (prompt("donner lvotre mdp "));
 let mailbdd = "coucou@moi.fr"
 let mdpbdd = "coucou"
 
-
 if((mail === mailbdd) && (mdp === mdpbdd)){
     console.log("bienvenue sur votre compte")
 
+} else if ((mail != mailbdd) && (mdp != mdpbdd)) {
+    console.log("info incorect")
+} else if (mdp != mdpbdd) {
+    console.log("mdp incorect")
 } else {
-    console.log("information incorect")
+    
+    console.log("mail incorect")
 }
+
+
+
+
+
 

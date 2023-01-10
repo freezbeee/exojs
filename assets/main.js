4e9ba68da83dc5a8f374d63c92dac7db4937af07
mich et marc <michelle.marco@gmail.com>
2023-01-10 11:05:27 +0100

    exo 1

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+let recup = prompt("votre prenom utilisateur 1 : ");
+let recupLastName = prompt("votre nom utilisateur 1 : ");
+let recup2 = prompt("votre prenom utilisateur 2 : ");
+let recupLastName2 = prompt("votre nom utilisateur 2 : ");
+
+let phrase = ` bonjour ${recup} ${recupLastName} et bojour a toi aussi 
+${recup2} ${recupLastName2} bivenue`
+
+console.log(phrase)

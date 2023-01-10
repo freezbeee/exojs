f01472238928f00618276f648c4e47bd3b7c3d27
theo courbot <michelle.marco@gmail.com>
2023-01-10 12:13:04 +0100

    1

--- exo3/assets/main.js
+++ exo3/assets/main.js
@@ -1,8 +1,8 @@
 let diametre = prompt("donner un dimetre en cm : "); 
 let rayon = parseInt(diametre) / 2;
 let result =2*Math.PI*rayon
-let resultfloor = Math.PI*Math.pow(rayon,2)
+let resultaire = Math.PI*Math.pow(rayon,2)
 
-let phrase = `  le perimetre d'un cercle de ${diametre} cm est ${result} cm et l'aire est ${resultfloor} cm2`
+let phrase = `  le perimetre d'un cercle de ${diametre} cm est ${result} cm et l'aire est ${resultair} cm25`
 
 console.log(phrase)

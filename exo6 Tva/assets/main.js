1f48f4b674ec61697208d668b7bfbe3a4e832a8e
theo courbot <michelle.marco@gmail.com>
2023-01-10 15:01:28 +0100

    1

--- exo6 Tva/assets/main.js
+++ exo6 Tva/assets/main.js
@@ -4,7 +4,7 @@ let tva =  (priceht * tvapourcent) / 100 ;
 let pricettc = priceht + tva
 
 
-let phrase1 = ` vous allez payer ${pricettc} euros `
+let phrase1 = ` vous allez payer ${pricettc} euros TTC`
 
 console.log(phrase1)
 

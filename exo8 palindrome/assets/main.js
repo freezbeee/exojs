c87143c9d61f6dd66cba64bb2bede549f468c429
theo courbot <michelle.marco@gmail.com>
2023-01-11 09:00:04 +0100

    1

---
+++ exo8 palindrome/assets/main.js
@@ -0,0 +1,21 @@
+
+let mot = prompt("donner un mot : ")
+
+
+
+
+    let motinverse = mot.split("").reverse("").join("");  
+  
+
+  
+  
+        
+        if (mot != motinverse) {  
+            alert( 'Non palindrome.');  
+        } else   {
+
+            alert( 'Palindrome');  
+        }
+    
+
+

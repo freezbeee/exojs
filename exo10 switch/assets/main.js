64ddd2102dc0c3117d36ce23807f5b656bef83a5
theo courbot <michelle.marco@gmail.com>
2023-01-11 10:33:32 +0100

    1

---
+++ exo10 switch/assets/main.js
@@ -0,0 +1,28 @@
+
+let choix =Number (prompt("votre choix : 1.eau 2.jus d'orange 3.limonade 4.cafe 5.thé" ))
+
+
+switch(true){
+    case choix === 1:
+        message = 'votre choix est "eau"'
+        break
+    case choix === 2:
+        message = "votre choix est 'jus d'orange'"
+        break
+    case choix === 3:
+        message = 'votre choix est "limonade"'
+        break
+    case choix === 4:
+        message = 'votre choix est "Café"'
+        break
+    case choix === 5:
+        message = 'votre choix est "Thé"'
+        break
+    default : 
+        message = "cette categorie n'existe pas"
+        break
+   
+
+
+}
+console.log(message)

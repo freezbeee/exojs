ff9d6e58b7824aaf06a0576e79541099ff6860ce
theo courbot <michelle.marco@gmail.com>
2023-01-18 08:49:15 +0100

    1

--- exo15/assets/main.js
+++ exo15/assets/main.js
@@ -20,24 +20,23 @@ let etudiants = [
         } 
     } 
 ]; 
-let moyenne = 0
-let nbnote = 0
 // console.log("liste des etudiants")
 // for(let key in etudiants){
-// console.log(key+" : " +etudiants[key]['prenom'] +" "+ etudiants[key]['nom'])
-   
-// }
-
-
-
-for(let key in etudiants){
-    console.log("etudiant")
-    console.log(key+" : " +etudiants[key]['prenom'] +" "+ etudiants[key]['nom'])
+    // console.log(key+" : " +etudiants[key]['prenom'] +" "+ etudiants[key]['nom'])
+    
+    // }
+    
+    
+    
+    for(let key in etudiants){
+        let moyenne = 0
+        let nbnote = 0
+        console.log("etudiant")
+        console.log(key+" : " +etudiants[key]['prenom'] +" "+ etudiants[key]['nom'])
 
     console.log("**********************")
     console.log("liste des matiere")
-    moyenne = 0
-    nbnote = 0
+
     for(let key2 in etudiants[key].matieres){
     console.log(`${[key2]}  ${etudiants[key].matieres[key2]}`)
     moyenne = moyenne + etudiants[key].matieres[key2]

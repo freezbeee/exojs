2f20f584c31e8a98669008fa9d57d78d7dabfafe
Your Name <courbot.theo@gmail.com>
2023-01-27 11:13:45 +0100

    exo obj

--- TP contact objet/exo 20 obj voiture et moto/script.js
+++ TP contact objet/exo 20 obj voiture et moto/script.js
@@ -1,13 +1,38 @@
-let name = document.querySelector("#name");
+let lastName = document.querySelector("#name");
 let firstname = document.querySelector("#firstname");
 let phone = document.querySelector("#phone");
 let birth = document.querySelector("#birth");
 let mail = document.querySelector("#mail");
 let btn = document.querySelector("#btn");
-
-console.log(renault.display());
-console.log(BMW.display());
 let text = document.querySelector("#text");
+// let form = document.querySelector("#form");
+let contact;
+console.log(mail.value);
+console.log(firstname);
 
-text.innerHTML += renault.display();
-text.innerHTML += BMW.display();
+btn.addEventListener("click", (e) => {
+  contact = new Contact(lastName, firstname, phone, birth, mail);
+  e.preventDefault();
+  text.innerHTML += contact.display();
+  console.log(contact.display());
+});
+class Contact {
+  constructor(lastName, firstname, phone, birth, mail) {
+    this.nom = lastName;
+    this.prenom = firstname;
+    this.telephone = phone;
+    this.naissance = birth;
+
+    this.email = mail;
+  }
+
+  display() {
+    return `<th> ${this.name}<th/> <th> ${this.firstname}<th/> <th> ${this.phone}<th/> <th> ${this.birth}<th/> <th> ${this.mail}<th/>`;
+  }
+}
+
+// console.log(renault.display());
+// console.log(BMW.display());
+
+// text.innerHTML += renault.display();
+// text.innerHTML += BMW.display();

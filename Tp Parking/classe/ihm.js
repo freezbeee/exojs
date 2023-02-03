138daa830470163637be6d3a5a11b5005e68249e
Your Name <courbot.theo@gmail.com>
2023-02-03 11:11:34 +0100

    exo

--- Tp Parking/classe/ihm.js
+++ Tp Parking/classe/ihm.js
@@ -9,9 +9,10 @@ export class Ihm {
   }
 
   demarrer() {
+    // console.log("hello");
     this.formulaire.addEventListener("submit", (e) => {
       e.preventDefault();
-      console.log(vehicules);
+      //   console.log(vehicules);
       console.log("vehicules");
       this.ajouter();
     });
@@ -24,12 +25,14 @@ export class Ihm {
     const voiture = new Vehicule(vehiculeInput);
     this.vehicules.push(voiture);
     this.afficher(voiture);
+    // console.log(voiture.imatriculation);
+    console.log(this.vehicules);
   }
 
-  afficher(c) {
+  afficher(c, d) {
     this.tableauHtmlResultat.innerHTML = `
     <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle rounded-3">
-  vehicule ${c.vehiculeInput} prenez votre ticket
+  vehicule ${c.imatriculation} prenez votre ticket
 </div>
         `;
   }
---
+++ Tp-photo-galery/asset/ts/main.ts
@@ -0,0 +1,97 @@
+class fish {
+  private static _count = 0;
+  private _id: number;
+  constructor(
+    private _firstname: string,
+    private _lastname: string,
+    private _Description: string,
+    private _url: string
+  ) {
+    this._id = ++fish._count;
+  }
+  get id() {
+    return this._id;
+  }
+
+  get firstname() {
+    return this._firstname;
+  }
+  get Lastname() {
+    return this.Lastname;
+  }
+  get Description() {
+    return this._Description;
+  }
+  get url() {
+    return this._url;
+  }
+  get retunFishList() {
+    return `${this._id}) ${this._firstname} ${this._lastname}`;
+  }
+}
+// console.log(fish);
+
+let fishTable = [
+  new fish(
+    "Nemo",
+    "Le Poisson Clown",
+    "la star des poisson s'est lui",
+    "./asset/img/Nemo.webp"
+  ),
+  new fish(
+    "Blop",
+    "Fish",
+    "l'un poisson le plus moche Avertissement risque de devenir aveugle",
+    "./asset/img/blop.jfif"
+  ),
+  new fish(
+    "Bubule",
+    "Le Poisson Rouge",
+    "Mellieur nom a donné a votre poisson",
+    "./asset/img/dori.jfif"
+  ),
+];
+let selectFish = fishTable[0];
+let myDivFishList = document.querySelector("#result-fish") as HTMLDivElement;
+
+const refreshDescription = () => {
+  let newpick = document.createElement("img");
+
+  let myDivFishDescription = document.querySelector(
+    "#result-description"
+  ) as HTMLDivElement;
+
+  if (selectFish) {
+    let fishPick = document.querySelector("#fishPick") as HTMLDivElement;
+    newpick.className = "card-img-top rounded-circle";
+    newpick.src = selectFish.url;
+    myDivFishDescription.innerHTML = selectFish.Description;
+
+    fishPick.appendChild(newpick);
+  } else {
+    myDivFishDescription.innerHTML = "";
+  }
+};
+
+const refreshFish = () => {
+  myDivFishList.innerHTML = "";
+  fishTable.forEach((fish) => {
+    let newButton = document.createElement("button");
+    newButton.textContent = fish.retunFishList;
+    newButton.className =
+      fish === selectFish
+        ? "btn btn-light p-2 px-4 w-100 my-2"
+        : "btn btn-outline-light p-2 px-4 w-100 my-2";
+    newButton.id = `selectFish-${fish.id}`;
+    newButton.addEventListener("click", () => {
+      selectFish = fish;
+      refreshDescription();
+      refreshFish();
+    });
+
+    myDivFishList.appendChild(newButton);
+  });
+};
+
+refreshFish();
+refreshDescription();
